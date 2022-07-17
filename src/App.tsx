b59fe728d2630ab14afb5fc44824ba3c13ce4053
Alexander Khapchenko <alexanderfox1337@gmail.com>
2022-07-17 23:58:33 +0300

    removal of excess

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router} from "react-router-dom";
 import {Routing} from "./components/containers";
-import {useLocalStorage} from "./components/hooks/useLocaleStorage";
 
 function App() {
   return (
     <Router>
-        {/*<Routing isAuthorized={Boolean(username)} setUsername={setUsername} removeUsername={removeUsername}/>*/}
         <Routing/>
     </Router>
   );
