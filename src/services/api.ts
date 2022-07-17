c11772a575664fc3cd10456e78c012ef03623701
Alexander Khapchenko <alexanderfox1337@gmail.com>
2022-07-17 23:58:52 +0300

    Added api service

---
+++ src/services/api.ts
@@ -0,0 +1,36 @@
+interface ITravelList {
+    createdAt: string
+    description: string
+    id: string
+    image: string
+    level: string
+    price: number
+    title: string
+}
+
+interface IBookingList {
+    "id": string,
+    "userId": string,
+    "tripId": string,
+    "guests": 2,
+    "date": string,
+    "trip": {
+        "title": string,
+        "duration": number,
+        "price": number
+    },
+    "totalPrice": number,
+    "createdAt": string
+}
+
+export const getTravelList = async (): Promise<Array<ITravelList>> => {
+    const response = await fetch('https://raw.githubusercontent.com/BinaryStudioAcademy/react-homework/production/homework/data/trips.json');
+    const result = await response.json();
+    return result;
+}
+
+export const getBookingList = async (): Promise<Array<IBookingList>> => {
+    const response = await fetch('https://raw.githubusercontent.com/BinaryStudioAcademy/react-homework/production/homework/data/bookings.json');
+    const result = await response.json();
+    return result;
+}
