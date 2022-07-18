55caf573cfda6e660d848c9c809603c54b0b8b4d
Alexander Khapchenko <alexanderfox1337@gmail.com>
2022-07-18 21:03:34 +0300

    Formated date

--- src/components/pages/bookings/booking-card/index.tsx
+++ src/components/pages/bookings/booking-card/index.tsx
@@ -10,11 +10,24 @@ interface IBookingCardProps {
 }
 
 const BookingCard: React.FC<IBookingCardProps> = ({title, date, quests, totalPrice, onDelete}) => {
+
+    const padTo2Digits = (num: number) => {
+        return num.toString().padStart(2, '0');
+    }
+
+    const formatDate = (date: Date) => {
+        return [
+            padTo2Digits(date.getDate()),
+            padTo2Digits(date.getMonth() + 1),
+            date.getFullYear(),
+        ].join('.');
+    }
+
     return (
         <li className={styles.booking}>
             <h3 className={styles.booking__title}>{title}</h3>
             <span>{quests} guests</span>
-            <span>{date}</span>
+            <span>{formatDate(new Date(date))}</span>
             <span>{totalPrice} $</span>
             <button className={styles.booking__cancel} title="Cancel booking" onClick={() => onDelete()}>
                 <span className={styles.visuallyHidden}>Cancel booking</span>
