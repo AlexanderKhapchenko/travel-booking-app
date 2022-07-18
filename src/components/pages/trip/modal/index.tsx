4641b4952ac6007ae5dd70a7d8a841f7002cc423
Alexander Khapchenko <alexanderfox1337@gmail.com>
2022-07-18 18:19:19 +0300

    Added modal

---
+++ src/components/pages/trip/modal/index.tsx
@@ -0,0 +1,89 @@
+import React, {useState} from "react";
+import styles from "./modal.module.scss";
+import Button from "../../../basic/button";
+import {Input} from "../../../basic";
+import {ITravel} from "../../../../hooks/interfaces";
+
+interface IModalProps {
+    onClose: Function,
+    card: ITravel,
+    addBooking: Function
+}
+
+const Modal: React.FC<IModalProps> = ({onClose, card, addBooking}) => {
+
+    const [date, setDate] = useState('');
+    const [guestsNumber, setGuestsNumber] = useState(1);
+
+    const validateDate = (newDate: string) => {
+        const nowDate = new Date();
+        const bookDate = new Date(newDate);
+        if(bookDate > nowDate) {
+            setDate(newDate);
+        } else {
+            alert("Date must be in future");
+        }
+    }
+
+    const onSubmit = () => {
+        if(date.length > 1 && guestsNumber >= 1 && guestsNumber <= 10) {
+            const newBooking = {
+                id: card.id,
+                userId: card.id,
+                tripId: card.id,
+                guests: guestsNumber,
+                date: date,
+                trip: {
+                    title: card.title,
+                    duration: card.duration,
+                    price: card.price
+                },
+                totalPrice: guestsNumber * card.price,
+                createdAt: new Date(),
+            }
+
+            addBooking(newBooking);
+        }
+    }
+
+    return (
+        <div className={styles.modal}>
+            <div className={styles.tripPopup}>
+                <button className={styles.tripPopup__close} onClick={() => onClose()}>×</button>
+                <form className={styles.tripPopup__form} autoComplete="off" onSubmit={onSubmit}>
+                    <div className={styles.tripInfo}>
+                        <h3 className={styles.tripInfo__title}>{card.title}</h3>
+                        <div className={styles.tripInfo__content}>
+                            <span className={styles.tripInfo__duration}><strong>{card.duration}</strong> days</span>
+                            <span className={styles.tripInfo__level}>{card.level}</span>
+                        </div>
+                    </div>
+                    <Input
+                        title="Date"
+                        name="date"
+                        type="date"
+                        required
+                        value={date}
+                        onChange={(e) => validateDate(e.target.value)}
+                    />
+                    <Input
+                        title="Number of guests"
+                        name="guests"
+                        type="number"
+                        min="1"
+                        max="10"
+                        required
+                        value={guestsNumber}
+                        onChange={(e) => setGuestsNumber(Number(e.target.value))}
+                    />
+                    <span className={styles.tripPopup__total}>
+                      Total: <output className={styles.tripPopup__totalValue}>{card.price * guestsNumber}$</output>
+                    </span>
+                    <Button type="submit">Book a trip</Button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Modal;
